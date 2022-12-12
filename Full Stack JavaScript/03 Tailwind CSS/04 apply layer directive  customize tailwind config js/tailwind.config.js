c8b5a9d1fbcd5c12e82b7fd4a4353b43eac15daa
RutvikShilamkar <90606251+RutvikShilamkar@users.noreply.github.com>
2022-12-12 22:07:47 +0530

    more on tailwind css

---
+++ Full Stack JavaScript/03 Tailwind CSS/04 apply layer directive  customize tailwind config js/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  // take reference of rutvikFullConfig,js file to add properties/classes in tailwind css 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
